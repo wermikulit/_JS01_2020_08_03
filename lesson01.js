ec772887c2f85da4888cb329e3803ed59514c582
wermikulit <elenav@mail.ru>
2020-08-03 11:58:34 +0500

    Первоначальный коммит

---
+++ lesson01.js
@@ -0,0 +1,11 @@
+let n1=123.45;
+let c1="c1";
+let null1=null;
+let undef1=undefined;
+let b1=true;
+/* alert(`type of n1=${n1} is ${typeof n1}`);
+alert(`type of c1=${c1} id ${typeof c1}`);
+alert(`type of null1=${null1} is ${typeof null1}`);
+alert(`type of undef1=${undef1} is ${typeof undef1}`);
+alert(`type of b1=${b1} is ${typeof b1}`); */
+alert("A"/2);
